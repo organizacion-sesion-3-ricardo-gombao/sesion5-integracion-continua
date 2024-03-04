045b979b3ee877a10b09c81e421d705f6135a2c2
Ricardo Gombao <157627401+rgombao@users.noreply.github.com>
2024-03-04 19:32:22 +0100

    Update Calculator.php

--- Calculator.php
+++ Calculator.php
@@ -1,6 +1,21 @@
 <?php
-class ParImpar
+class Calculator
 {
+ 
+    public function add($a, $b): float
+    {
+        return $a + $b;
+    }
+
+    public function multiply($a, $b): float
+    {
+        return $a * $b;
+    }
+
+    public function division($a, $b): float
+    {
+        return $a / $b;
+    }
  
     public function esPar($a)
     {
