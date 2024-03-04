b723fa4b396c2c8b0a072000af31c21106911cdb
Ricardo Gombao <157627401+rgombao@users.noreply.github.com>
2024-03-04 19:34:09 +0100

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,8 +2,38 @@
 use PHPUnit\Framework\TestCase;
 require 'Calculator.php';
 
-class ParImparTests extends TestCase
+class CalculatorTest extends TestCase
 {
+    private $calculator;
+ 
+    protected function setUp(): void
+    {
+        $this->calculator = new Calculator();
+    }
+ 
+    protected function tearDown(): void
+    {
+        $this->calculator = NULL;
+    }
+ 
+    public function testAdd(): void
+    {
+        $result = $this->calculator->add(1, 2);
+        $this->assertEquals(3, $result);
+    }
+
+    public function testMultiply(): void
+    {
+        $result = $this->calculator->multiply(5, 2);
+        $this->assertEquals(10, $result);
+    }
+
+    public function testDivision(): void
+    {
+        $result = $this->calculator->division(10, 2);
+        $this->assertEquals(5, $result);
+    }
+ 
     private $parImpar;
  
     protected function setUp()
