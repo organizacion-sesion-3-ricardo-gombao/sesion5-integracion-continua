2b707cbddd3d1e2c8e4ef382eac810d2a1bf2f7b
Ricardo Gombao <157627401+rgombao@users.noreply.github.com>
2024-03-05 10:42:15 +0100

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -37,25 +37,25 @@ class CalculatorTest extends TestCase
    
     public function testPar()
     {
-        $result = $this->parImpar->esPar(2);
+        $result = $this->calculator->esPar(2);
         $this->assertEquals(true, $result);
     }
 
     public function testPar2()
     {
-        $result = $this->parImpar->esPar(3);
+        $result = $this->calculator->esPar(3);
         $this->assertEquals(false, $result);
     } 
     
 	public function testImpar()
     {
-        $result = $this->parImpar->esImpar(2);
+        $result = $this->calculator->esImpar(2);
         $this->assertEquals(false, $result);
     }
 
     public function testImpar2()
     {
-        $result = $this->parImpar->esImpar(3);
+        $result = $this->calculator->esImpar(3);
         $this->assertEquals(true, $result);
     } 
  
